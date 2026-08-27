71b9b6d79198aa4d0a8584db90e5c26234abd6db
KomalMaurya <komal092btcseai23@igdtuw.ac.in>
2026-08-27 08:33:19 +0530

    First Problem

---
+++ Introductory Problems/Weird_Algorithm.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define yes cout << "YES\n"
+#define no  cout << "NO\n"
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+
+
+
+void solve() {
+    // your code here
+    ll n;
+    cin >> n;
+    while(true){
+        cout << n << " ";
+        if(n == 1) break;
+        if(n & 1){
+            n = n * 3 + 1;
+        }else{
+            n = n / 2;
+        }
+    }
+    cout << "\n";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;         
+    while (t--) solve();
+
+    return 0;
+}
